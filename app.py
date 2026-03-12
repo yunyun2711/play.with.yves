99b95da81c95638a1eba44a884c57c3002aeb42b
yunyun2711 <yuneysiramon@gmail.com>
2026-03-11 19:36:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -761,4 +761,5 @@ def dashboard():
 
     return html
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
